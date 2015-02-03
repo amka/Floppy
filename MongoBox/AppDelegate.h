401b56952b7f4e5b0e25a4417146b14491f59853
Andrey M <meamka@ya.ru>
2015-02-03 15:32:43 +0300

    Initial Commit

---
+++ MongoBox/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MongoBox
+//
+//  Created by Andrey M on 03.02.15.
+//  Copyright (c) 2015 Andrey M. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ MongoBox/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  MongoBox
+//
+//  Created by Andrey M on 03.02.15.
+//  Copyright (c) 2015 Andrey M. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
